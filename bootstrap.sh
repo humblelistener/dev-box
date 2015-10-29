868b85d92735140ad59f4ec63e7a599cbb57d1bd
Karthik Padmanabhan {PK} <karthi.email@gmail.com>
2015-10-30 00:54:47 +1100

    more bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 apt-get update
+apt-get dist-upgrade -y
 apt-get install -y curl
+
+apt-get install git
 
 # programming languages
 apt-get install -y nodejs
 apt-get install -y golang
 
 # docker
-apt-get install docker-engine
+uname -r
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-vivid main" >> /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get purge lxc-docker* -y
+apt-get install -y docker-engine
+sudo systemctl enable docker
