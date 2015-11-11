854c4cdbfb4f705a7cd1072889cbfc403143676a
Karthik Padmanabhan {PK} <karthi.email@gmail.com>
2015-11-11 16:47:19 +1100

    fixing docker script

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,10 +14,12 @@ sudo true
 sudo apt-get -y install linux-image-extra-$(uname -r)
 
 # Add Docker PPA and install latest version
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-vivid main" | sudo tee -a /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install lxc-docker -y
+sudo apt-get purge lxc-docker*
+sudo apt-cache policy docker-engine
+sudo apt-get install -y docker-engine
 sudo groupadd docker
 sudo gpasswd -a vagrant docker
 sudo service docker restart
