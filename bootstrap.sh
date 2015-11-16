4dd9c31c39989b1421b5283483c5d4a6b0931b1b
P Karthikeyan <karthi.email@gmail.com>
2015-11-16 12:02:22 +1100

    simplifyuing the bootstrap to do jus what we need now

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,47 +29,5 @@ sudo sh -c "curl -L https://github.com/docker/compose/releases/download/1.4.2/do
 sudo chmod +x /usr/local/bin/docker-compose
 sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/1.4.2/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
-# Install docker-cleanup command
-cd /tmp
-git clone https://gist.github.com/76b450a0c986e576e98b.git
-cd 76b450a0c986e576e98b
-sudo mv docker-cleanup /usr/local/bin/docker-cleanup
-sudo chmod +x /usr/local/bin/docker-cleanup
-
-# programming languages
-apt-get install -y nodejs
-apt-get install -y golang
-
-# install ruby
-echo "install ruby"
-sudo apt-get install -y ruby-full
-sudo gem install bundler
-
-# install dnvm
-curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh
-sudo -s source ~/.dnx/dnvm/dnvm.sh
-
-# install DNX for .NET Core
-sudo apt-get install -y libunwind8 gettext libssl-dev libcurl3-dev zlib1g libicu-dev
-dnvm upgrade -r coreclr
-
-# install DNX for Mono
-# install mono ref: http://www.mono-project.com/docs/getting-started/install/linux/#debian-ubuntu-and-derivatives
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-sudo apt-get update
-dnvm upgrade -r mono
-
-# install libuv
-sudo apt-get install -y make automake libtool curl
-curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxfv - -C /usr/local/src
-cd /usr/local/src/libuv-1.4.2
-sudo sh autogen.sh
-sudo ./configure
-sudo make
-sudo make install
-sudo rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
-sudo ldconfig
-
 # restart the machine finally
 sudo reboot
