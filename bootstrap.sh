3ed971ca49afdb3b226fbbd1f22b9ee7c60992dd
P Karthikeyan <karthi.email@gmail.com>
2016-01-07 16:39:32 +1100

    fixing the ecs cli

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,6 +35,7 @@ dnvm upgrade -r coreclr
 
 # aws ecs cli
 sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
+sudo chmod +x /usr/local/bin/ecs-cli
 
 # restart the machine finally
 sudo reboot
