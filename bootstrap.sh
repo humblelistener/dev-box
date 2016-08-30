f20503b01a3ca9a18fa05633d4da5421680b4fc0
P Karthikeyan <karthi.email@gmail.com>
2016-08-30 15:20:00 +1000

    fixing some issues

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,9 @@ sudo apt-get install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
+apt-cache policy docker-engine
+sudo apt-get purge lxc-docker
+sudo apt-get update
 
 echo "--------------- installing linux-image-extra package, this allows use of aufs storage driver ---------------"
 sudo apt-get install -yy linux-image-extra-$(uname -r)
